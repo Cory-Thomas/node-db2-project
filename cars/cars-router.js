8c64ce5c71b915b455804014402127bab7b3c199
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-06 18:21:58 -0400

    playing around with async/await

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -5,32 +5,44 @@ const db = require( "../data/connection.js" );
 const router = express.Router();
 
 
-router.get( "/", (req, res) => {
-    db( "cars" )
-        .then(cars => {
-            res.status(200).json({cars});
-        })
-        .catch(err => {
-            res.status(500).json({ message: "Failed to retrieve cars", error: err.message });
-        });
-});
+// router.get( "/", (req, res) => {
+//     db( "cars" )
+//         .then(cars => {
+//             res.status(200).json({data: cars});
+//         })
+//         .catch(error => {
+//             handleError(error, res);
+//         });
+// });
 
+router.get( "/", async (req, res) => {
+    try {
+        res.status(200).json( await db( "cars" ) );
+    } catch(error) {
+            handleError(error, res);
+    };
+});
 
 router.post( "/", (req, res) => {
     const carData = req.body;
     db( "cars" )
-        .insert(carData)
-        .then(ids => {
-            db("cars")
-                .where({ id: ids[0] })
-                .then(newCarEntry => {
-                    res.status(201).json(newCarEntry);
-                });
-        })
-        .catch(err => {
-            console.log("POST error", err);
-            res.status(500).json({ message: "Failed to store data" });
-        });
+        .insert(carData, "id")
+            .then(ids => {
+                db("cars")
+                    .where({ id: ids[0] })
+                    .first()
+                    .then(newCarEntry => {
+                        res.status(201).json({ data: newCarEntry });
+                    });
+            })
+            .catch(error => {
+                handleError(error, res);
+            });
 });
+
+function handleError(error, res) {
+    console.log("error", error);
+    res.status(500).json({ message: error.message });
+}
 
 module.exports = router; 
